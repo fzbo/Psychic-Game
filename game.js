4084126b34867ecd9ed8d4e664a1d30dfcbb9a05
fzbo <elizabeth.bazan@gmail.com>
2017-11-10 21:29:25 -0500

    Homework 3

---
+++ game.js
@@ -0,0 +1,73 @@
+
+
+
+//CHOICES AVAILABLE
+var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+//Setting var to zero
+var wins = 0;
+var losses = 0;
+var guesses = 9;
+var guessesLeft = 9;
+var guessedLetters = [];
+var letterToGuess = null;
+
+
+
+//COMPUTER WILL SELECT A RANDOM LETTER. 
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+//USER ALLOWED 9 GUESSES
+
+var updateGuessesLeft = function() {
+  // CALLING ON THE HTML ELEMENT AND SETTING IT EQUAL TO THE GUESSES LEFT.
+  document.querySelector('#guessLeft').innerHTML = "Guesses left: " + guessesLeft;
+};
+
+var updateLetterToGuess = function() {
+  this.letterToGuess = this.computerChoices[Math.floor(Math.random() * this.computerChoices.length)];
+};
+var updateGuessesSoFar = function() {
+  //GUESSES THE USER HAS TRIED. 
+  document.querySelector('#let').innerHTML = "Your Guesses so far: " + guessedLetters.join(', ');
+};
+// FUNCTION WILL BE CALLED WHEN WE RESET.
+var reset = function() {
+  totalGuesses = 9;
+  guessesLeft = 9;
+  guessedLetters = [];
+
+  updateLetterToGuess();
+  updateGuessesLeft();
+  updateGuessesSoFar();
+}
+
+updateLetterToGuess();
+updateGuessesLeft();
+
+
+//WHEN KEY IS RELEASED BECOMES USERS GUESS.
+document.onkeyup = function(event) {
+    guessesLeft--;
+  var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+  guessedLetters.push(userGuess);
+  updateGuessesLeft();
+  updateGuessesSoFar();
+
+        if (guessesLeft > 0){
+            if (userGuess == letterToGuess){
+                wins++;
+                document.querySelector('#wins').innerHTML = "Wins: " + wins;
+                alert("Yes, you are psychic!");
+                reset();
+            }
+        }else if(guessesLeft == 0){
+            // WE LOSE AND CODE WILL UPDATE HTML TO DISPLAY LOSS.
+            losses++;
+            document.querySelector('#losses').innerHTML = "Losses: " + losses;
+            alert("Sorry, you're not psychic, maybe try again?");
+            // Then we'll call the reset. 
+            reset();
+        }
+};
